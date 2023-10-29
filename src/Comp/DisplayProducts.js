fb70b1d2ada93a5b82e1a4916d623ec4c286b20d
Abhishek kumar <90932403+meabhiarya1@users.noreply.github.com>
2023-10-29 10:47:45 +0530

    button-color

--- src/Comp/DisplayProducts.js
+++ src/Comp/DisplayProducts.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Table } from "react-bootstrap";
 import "./DisplayProducts.css";
+import { MDBIcon } from 'mdb-react-ui-kit';
 
 const DisplayProducts = ({ products }) => {
   let initialState = [];
@@ -28,78 +29,82 @@ const DisplayProducts = ({ products }) => {
 
   }
 
+  const delQty = () =>{
+
+  }
+
   return (
-    <div className="table">
-      <Table striped bordered hover variant="dark" >
-        <thead>
-          <tr>
-            <th>Index</th>
-            <th>Product Name</th>
-            <th>Price</th>
-            <th>Height</th>
-            <th>Width</th>
-            <th>Thickness</th>
-            <th>Kilo</th>
-            <th>Grams</th>
-            <th>Quantity</th>
-            <th>Options</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{index+1}</td>
-                <td>{item.product_name}</td>
-                <td>{item.price}</td>
-                <td>{item.height}</td>
-                <td>{item.width}</td>
-                <td>{item.thick}</td>
-                <td>{item.kilo}</td>
-                <td>{item.grams}</td>
-                <td>{item.quantity}</td>
-                <td>
-                  <Button
-                    variant="success"
-                    onClick={(event) => increQty(event)}
-                    value={index}
-                    className="button"
-                  >
-                    +
-                  </Button>
-                  <Button
-                    variant="danger"
-                    onClick={(event) => decreQty(event)}
-                    value={index}
-                    className="button"
-                  >
-                    -
-                  </Button>
+      <div className="table">
+        <Table striped bordered hover variant="dark" >
+          <thead>
+            <tr>
+              <th>Index</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Height</th>
+              <th>Width</th>
+              <th>Thickness</th>
+              <th>Kilo</th>
+              <th>Grams</th>
+              <th>Quantity</th>
+              <th>Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index+1}</td>
+                  <td>{item.product_name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.height}</td>
+                  <td>{item.width}</td>
+                  <td>{item.thick}</td>
+                  <td>{item.kilo}</td>
+                  <td>{item.grams}</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    <Button
+                      variant="success"
+                      onClick={(event) => increQty(event)}
+                      value={index}
+                      className="button"
+                    >
+                      +
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={(event) => decreQty(event)}
+                      value={index}
+                      className="button"
+                    >
+                      -
+                    </Button>
 
-                  <Button
-                    variant="primary"
-                    onClick={(event) => editQty(event)}
-                    value={index}
-                    className="button"
-                  >
-                    Edit
-                  </Button>
+                    <Button
+                      variant="primary"
+                      onClick={(event) => editQty(event)}
+                      value={index}
+                      className="button"
+                    >
+                      Edit
+                    </Button>
 
-                  <Button
-                    variant="primary"
-                    onClick={(event) => editQty(event)}
-                    value={index}
-                    className="button"
-                  >
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-    </div>
+                    <Button
+                      variant="primary"
+                      onClick={(event) => delQty(event)}
+                      value={index}
+                      className="button"
+                    >
+                      <MDBIcon fas icon="trash" />
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
   );
 };
 
