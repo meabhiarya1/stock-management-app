e78ed01da5fee27ed8a6803686859155ce28d81a
Abhishek kumar <90932403+meabhiarya1@users.noreply.github.com>
2023-10-31 16:06:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; //React Bootstrap
-import NavBar from './Comp/NavBar';
+import AddProduct from './Comp/AddProduct';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
+        <AddProduct/>
       </header>
     </div>
   )
